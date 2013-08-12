d29411806c5c2d0ea93cbed95e51abfac43ee51c
Kevin Vicrey <kevin.vicrey@nexu.org>
2013-08-11 21:18:39 -0400

    Code

---
+++ app.js
@@ -0,0 +1,90 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+//==================================================================
+// Define the strategy to be used by PassportJS
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    if (username === "admin" && password === "admin") // stupid example
+      return done(null, {name: "admin"});
+
+    return done(null, false, { message: 'Incorrect username.' });
+  }
+));
+
+// Serialized and deserialized methods when got from session
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+// Define a middleware function to be used for every secured routes
+var auth = function(req, res, next){
+  if (!req.isAuthenticated()) 
+  	res.send(401);
+  else
+  	next();
+};
+//==================================================================
+
+// Start express application
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.cookieParser()); 
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.session({ secret: 'securedsession' }));
+app.use(passport.initialize()); // Add passport initialization
+app.use(passport.session());    // Add passport initialization
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+//==================================================================
+// routes
+app.get('/', function(req, res){
+  res.render('index', { title: 'Express' });
+});
+
+app.get('/users', auth, function(req, res){
+  res.send([{name: "user1"}, {name: "user2"}]);
+});
+//==================================================================
+
+//==================================================================
+// route to test if the user is logged in or not
+app.get('/loggedin', function(req, res) {
+  res.send(req.isAuthenticated() ? req.user : '0');
+});
+
+// route to log in
+app.post('/login', passport.authenticate('local'), function(req, res) {
+  res.send(req.user);
+});
+
+// route to log out
+app.post('/logout', function(req, res){
+  req.logOut();
+  res.send(200);
+});
+//==================================================================
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ public/javascripts/app.js
@@ -0,0 +1,129 @@
+'use strict';
+
+/**********************************************************************
+ * Angular Application
+ **********************************************************************/
+var app = angular.module('app', ['ngResource'])
+  .config(function($routeProvider, $locationProvider, $httpProvider) {
+    //================================================
+    // Check if the user is connected
+    //================================================
+    var checkLoggedin = function($q, $timeout, $http, $location, $rootScope){
+      // Initialize a new promise
+      var deferred = $q.defer();
+
+      // Make an AJAX call to check if the user is logged in
+      $http.get('/loggedin').success(function(user){
+        // Authenticated
+        if (user !== '0')
+          $timeout(deferred.resolve, 0);
+
+        // Not Authenticated
+        else {
+          $rootScope.message = 'You need to log in.';
+          $timeout(function(){deferred.reject();}, 0);
+          $location.url('/login');
+        }
+      });
+
+      return deferred.promise;
+    };
+    //================================================
+    
+    //================================================
+    // Add an interceptor for AJAX errors
+    //================================================
+    $httpProvider.responseInterceptors.push(function($q, $location) {
+      return function(promise) {
+        return promise.then(
+          // Success: just return the response
+          function(response){
+            return response;
+          }, 
+          // Error: check the error status to get only the 401
+          function(response) {
+            if (response.status === 401)
+              $location.url('/login');
+            return $q.reject(response);
+          }
+        );
+      }
+    });
+    //================================================
+
+    //================================================
+    // Define all the routes
+    //================================================
+    $routeProvider
+      .when('/', {
+        templateUrl: '/views/main.html'
+      })
+      .when('/admin', {
+        templateUrl: 'views/admin.html',
+        controller: 'AdminCtrl',
+        resolve: {
+          loggedin: checkLoggedin
+        }
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+    //================================================
+
+  }) // end of config()
+  .run(function($rootScope, $http){
+    $rootScope.message = '';
+
+    // Logout function is available in any pages
+    $rootScope.logout = function(){
+      $rootScope.message = 'Logged out.';
+      $http.post('/logout');
+    };
+  });
+
+
+/**********************************************************************
+ * Login controller
+ **********************************************************************/
+app.controller('LoginCtrl', function($scope, $rootScope, $http, $location) {
+  // This object will be filled by the form
+  $scope.user = {};
+
+  // Register the login() function
+  $scope.login = function(){
+    $http.post('/login', {
+      username: $scope.user.username,
+      password: $scope.user.password,
+    })
+    .success(function(user){
+      // No error: authentication OK
+      $rootScope.message = 'Authentication successful!';
+      $location.url('/admin');
+    })
+    .error(function(){
+      // Error: authentication failed
+      $rootScope.message = 'Authentication failed.';
+      $location.url('/login');
+    });
+  };
+});
+
+
+
+/**********************************************************************
+ * Admin controller
+ **********************************************************************/
+app.controller('AdminCtrl', function($scope, $http) {
+  // List of users got from the server
+  $scope.users = [];
+
+  // Fill the array to display it in the page
+  $http.get('/users').success(function(users){
+    for (var i in users)
+      $scope.users.push(users[i]);
+  });
+});
